e1282093c9eaaa95786e0690433310d675b66619
Peter Treitler <peter.treitler@gmail.com>
2020-04-27 22:22:00 +0200

    Add types to 01_intro

--- src/01_intro.ts
+++ src/01_intro.ts
@@ -15,7 +15,7 @@ export function addNumbers(x: number, y: number): number {
 // both strings and numbers, the goal of this function is to add strings.
 // By enforcing this in the function signature, we can safely change
 // its implementation later
-export function addStrings(x, y) {
+export function addStrings(x: string, y: string) {
   return x + y;
 }
 
@@ -35,7 +35,7 @@ export function addStrings(x, y) {
 // This function can accept any type!
 // But it always returns something of the same type as what was provided.
 // Hint: Use a generic type (<T>).
-export function identity(item) {
+export function identity<T>(item: T): T {
   return item;
 }
 
@@ -43,31 +43,33 @@ export function identity(item) {
 // attempt applies the passed in function with the supplied arguments. If the
 // function throws an error, the error is being returned. If the function does
 // not throw an error, the result is being returned.
-export function attempt(func, ...args) {
+export function attempt(func: Function, ...args: Array<any>) {
   try {
     return func(...args);
-  } catch(err) {
+  } catch (err) {
     return err;
   }
 }
 
 // ### constant
 // constant returns a function that returns a the passed in value.
-export function constant(value) {
-  return function() {
+export function constant<T>(value: T): () => T {
+  return function (): T {
     return value;
-  }
+  };
 }
 
 // ### noop
 // noop can be called with arbitrary arguments, it will always return
 // `undefined`.
-export function noop() {}
+export function noop(): void {}
 
 // ### times
 // times invokes the passed in iteratee (2nd argument) n times. It returns an
 // array of results.
-export function times(n, iteratee) {
+export function times<T>(n: number, iteratee: Function): Array<T> {
   // If the fill function doesn't exist then implement it...
-  return Array(n).fill().map((o, i) => iteratee(i));
+  return Array(n)
+    .fill(0)
+    .map((o, i) => iteratee(i));
 }
